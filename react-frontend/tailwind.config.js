0b38409ef0fafd3da1114095d95348e547ed83b8
Your Name <you@example.com>
2026-02-08 11:01:05 -0600

    updated files

--- react-frontend/tailwind.config.js
+++ react-frontend/tailwind.config.js
@@ -1,4 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -8,4 +13,5 @@ export default {
       }
     },
   },
+  plugins: [],
 }
